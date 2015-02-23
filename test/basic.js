683cb19c07fbbcc106f800525c9c6aaa776291f0
hex7c0 <carniellifrancesco@gmail.com>
2015-02-23 20:04:13 +0100

    test: fix tab size add writable test add more comment

--- test/basic.js
+++ test/basic.js
@@ -2,7 +2,6 @@
 /**
  * @file basic test
  * @module setheaders
- * @package setheaders
  * @subpackage test
  * @version 0.0.1
  * @author hex7c0 <hex7c0@gmail.com>
@@ -13,108 +12,155 @@
  * initialize module
  */
 // import
-try {
-    var setHeader = require('..');
-    var assert = require('assert');
-    var http = require('http');
-    var request = require('supertest');
-} catch (MODULE_NOT_FOUND) {
-    console.error(MODULE_NOT_FOUND);
-    process.exit(1);
-}
+var setHeader = require('..');
+var assert = require('assert');
+var http = require('http');
+var request = require('supertest');
 
 /*
  * test module
  */
-describe('basic', function() {
+describe(
+  'basic',
+  function() {
 
     var app;
     describe('normal', function() {
 
-        before(function(done) {
+      before(function(done) {
 
-            app = http.createServer(function(req, res) {
+        app = http.createServer(function(req, res) {
 
-                assert.equal(setHeader(res, 'ciao', 'pippo'), true);
-                assert.equal(res.getHeader('ciao'), 'pippo');
+          assert.equal(setHeader(res, 'ciao', 'pippo',
+            'should set normal header'), true);
+          assert.equal(res.getHeader('ciao'), 'pippo');
 
-                assert.equal(setHeader(res, 'ciao', 'pizza'), true);
-                assert.equal(res.getHeader('ciao'), 'pizza');
+          assert.equal(setHeader(res, 'ciao', 'pizza'), true);
+          assert.equal(res.getHeader('ciao'), 'pizza');
 
-                assert.equal(setHeader(res, 'PippO', 'foo'), true);
+          assert.equal(setHeader(res, 'PippO', 'foo'), true);
 
-                res.writeHead(200, {
-                    'Content-Type': 'text/plain'
-                });
-                res.end();
-            });
-            done();
+          res.writeHead(200, {
+            'Content-Type': 'text/plain'
+          });
+          res.end();
         });
+        done();
+      });
 
-        it('should return 2 headers', function(done) {
+      it('should return 2 headers', function(done) {
 
-            request(app).get('/').expect('ciao', 'pizza')
-                    .expect('PippO', 'foo').expect(200, done);
-        });
+        request(app).get('/').expect('ciao', 'pizza').expect('PippO', 'foo')
+        .expect(200, done);
+      });
     });
 
     describe('protect', function() {
 
+      before(function(done) {
+
+        app = http.createServer(function(req, res) {
+
+          assert.equal(setHeader(res, 'ciao', 'pippo', true), true,
+            'should protect this header from being overridden');
+          assert.equal(res.getHeader('ciao'), 'pippo');
+
+          setHeader(res, 'ciao', 'pizza');
+          assert.equal(res.getHeader('ciao'), 'pippo',
+            'I can\'t change this header :(');
+
+          assert.equal(setHeader(res, 'PippO', 'foo'), true);
+
+          res.writeHead(200, {
+            'Content-Type': 'text/plain'
+          });
+          res.end();
+        });
+        done();
+      });
+
+      it('should return 2 headers', function(done) {
+
+        request(app).get('/').expect('ciao', 'pippo').expect('PippO', 'foo')
+        .expect(200, done);
+      });
+    });
+
+    describe(
+      'override',
+      function() {
+
         before(function(done) {
 
-            app = http.createServer(function(req, res) {
+          app = http
+          .createServer(function(req, res) {
 
-                assert.equal(setHeader(res, 'ciao', 'pippo', true), true);
-                assert.equal(res.getHeader('ciao'), 'pippo');
+            assert.equal(setHeader(res, 'ciao', 'pippo'), true);
+            assert.equal(res.getHeader('ciao'), 'pippo');
 
-                assert.equal(setHeader(res, 'ciao', 'pizza'), false);
-                assert.equal(res.getHeader('ciao'), 'pippo');
+            assert.equal(setHeader(res, 'ciao', 'pizza', null, true,
+              'I cant\'t override this header if present'), true);
+            assert.equal(res.getHeader('ciao'), 'pippo',
+              'should get previous header');
 
-                assert.equal(setHeader(res, 'PippO', 'foo'), true);
+            assert.equal(setHeader(res, 'PippO', 'foo',
+              'should override previous header, because no option is set'),
+              true);
 
-                res.writeHead(200, {
-                    'Content-Type': 'text/plain'
-                });
-                res.end();
+            res.writeHead(200, {
+              'Content-Type': 'text/plain'
             });
-            done();
+            res.end();
+
+            assert
+            .equal(setHeader(res, 'ciao', 'pippo', null, true), true,
+              'I cant\'t override this header if present, even if socket is closed');
+          });
+          done();
         });
 
         it('should return 2 headers', function(done) {
 
-            request(app).get('/').expect('ciao', 'pippo')
-                    .expect('PippO', 'foo').expect(200, done);
+          request(app).get('/').expect('ciao', 'pippo').expect('PippO', 'foo')
+          .expect(200, done);
         });
+      });
+
+    describe('writable', function() {
+
+      before(function(done) {
+
+        app = http.createServer(function(req, res) {
+
+          assert.equal(setHeader(res, 'ciao', 'pippo'), true);
+          assert.equal(res.getHeader('ciao'), 'pippo');
+
+          assert.equal(setHeader(res, 'PippO', 'foo', null, null, true), true);
+
+          res.writeHead(200, {
+            'Content-Type': 'text/plain'
+          });
+          res.end();
+
+          assert.equal(setHeader(res, 'ciao', 'pippo', null, null, true),
+            false, 'should return false because socket is sent');
+
+          // hack
+          res._headerSent = false;
+          try {
+            assert.equal(setHeader(res, 'ciao', 'pippo', null, null, true),
+              false, 'should return Exception because socket is sent');
+          } catch (e) {
+            assert.equal(e.message, 'Can\'t set headers after they are sent.');
+          }
+        });
+        done();
+      });
+
+      it('should return 2 headers', function(done) {
+
+        request(app).get('/').expect('ciao', 'pippo').expect('PippO', 'foo')
+        .expect(200, done);
+      });
     });
-
-    describe('override', function() {
-
-        before(function(done) {
-
-            app = http
-                    .createServer(function(req, res) {
-
-                        assert.equal(setHeader(res, 'ciao', 'pippo'), true);
-                        assert.equal(res.getHeader('ciao'), 'pippo');
-
-                        assert
-                                .equal(setHeader(res, 'ciao', 'pizza', false, false), true);
-                        assert.equal(res.getHeader('ciao'), 'pippo');
-
-                        assert.equal(setHeader(res, 'PippO', 'foo'), true);
-
-                        res.writeHead(200, {
-                            'Content-Type': 'text/plain'
-                        });
-                        res.end();
-                    });
-            done();
-        });
-
-        it('should return 2 headers', function(done) {
-
-            request(app).get('/').expect('ciao', 'pippo')
-                    .expect('PippO', 'foo').expect(200, done);
-        });
-    });
-});
+  });
