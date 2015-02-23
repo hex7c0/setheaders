326d10890e56ba6ba5789fbbd163a18f7c6b4b41
hex7c0 <carniellifrancesco@gmail.com>
2015-02-23 19:55:42 +0100

    main: add more comment add writable argument

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 /**
  * @file setheaders main
  * @module setheaders
- * @package setheaders
  * @subpackage main
- * @version 0.0.2
+ * @version 0.1.0
  * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
  * @license GPLv3
@@ -22,38 +21,54 @@
  * @param {String} value - header's value
  * @param {Boolean} [protect] - set protected header
  * @param {Boolean} [override] - check if I'm trying to override a header
+ * @param {Boolean} [writable] - check if socket is writable, prevent write Error
  * @return {Boolean}
  */
-function setHeader(res, name, value, protect, override) {
+function setHeader(res, name, value, protect, override, writable) {
 
-  var low = name.toLowerCase();
-
-  if (override === false
-    && (res._headers !== undefined && res._headers[name] !== undefined)) {
-    return true;
+  // check an override
+  if (override === true) {
+    var isPresent = res._headers && res._headers[name] !== undefined;
+    if (isPresent === true) {
+      return true;
+    }
   }
+  // I don't care or not found
+
+  // is socket writable?
+  if (writable === true) {
+    var isWritable = res._headerSent === false || res.finished === false;
+    if (isWritable === false) {
+      return false;
+    }
+  }
+  // it's dangerous to go alone
 
   res.setHeader(name, value);
 
-  var previous = Object.getOwnPropertyDescriptor(res._headers, low);
-  if (previous !== undefined && previous.configurable === false) {
-    return false;
-  }
+  /**
+   * @deprecated
+   * 
+   * var previous = Object.getOwnPropertyDescriptor(res._headers, low); if (previous !== undefined && previous.configurable === false) { return false; }
+   */
 
+  // do you want to lock this header?
   if (protect === true) {
+    var low = name.toLowerCase();
     Object.defineProperty(res._headers, low, {
       configurable: false,
       enumerable: true,
-      get: function() {
+      get: function() { // getter
 
         return value;
       },
-      set: function() {
+      set: function() { // setter
 
         return; // prevent set error
       }
     });
   }
+  // okay easy rider
 
   return true;
 }
